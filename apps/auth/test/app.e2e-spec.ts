8514f3c2b4c9adf7be7ce60c1750ec4e05f03c19
jhordy <jhooordy@gmail.com>
2024-01-30 19:24:49 -0300

    chore: e2e tests for auth

--- apps/auth/test/app.e2e-spec.ts
+++ apps/auth/test/app.e2e-spec.ts
@@ -2,9 +2,11 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AuthModule } from './../src/auth.module';
+import { PrismaService } from '../src/providers/prisma.service';
 
-describe('AuthController (e2e)', () => {
+describe('AuthModule (e2e)', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -13,12 +15,62 @@ describe('AuthController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    prisma = moduleFixture.get<PrismaService>(PrismaService);
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await prisma.user.deleteMany();
+    await app.close();
+  });
+
+  it('should sign up a user successfully', async () => {
+    const signUpMutation = `
+      mutation {
+        signUp(
+          signUpInput: {
+            name: "Test User Macapa"
+            email: "test_user@macapa.com"
+            password: "123456"
+          }
+        ) {
+          id
+          name
+          email
+        }
+      }
+    `;
+
+    const response = await request(app.getHttpServer())
+      .post('/graphql')
+      .send({ query: signUpMutation })
+      .expect(200);
+
+    const { data } = response.body;
+    expect(data).toBeDefined();
+    expect(data.signUp).toBeDefined();
+    expect(data.signUp.id).toBeDefined();
+    expect(data.signUp.name).toBe('Test User Macapa');
+    expect(data.signUp.email).toBe('test_user@macapa.com');
+  });
+
+  it('should sign in a user successfully and return an access token', async () => {
+    const signInMutation = `
+      mutation {
+        signIn(signInInput: { email: "test_user@macapa.com", password: "123456" }) {
+          accessToken
+        }
+      }
+    `;
+
+    const response = await request(app.getHttpServer())
+      .post('/graphql')
+      .send({ query: signInMutation })
+      .expect(200);
+
+    const { data } = response.body;
+    expect(data).toBeDefined();
+    expect(data.signIn).toBeDefined();
+    expect(data.signIn.accessToken).toBeDefined();
   });
 });
