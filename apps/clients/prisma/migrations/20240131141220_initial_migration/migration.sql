2c10323562734564d4593f6232b676c2d667f121
jhordy <jhooordy@gmail.com>
2024-01-31 11:19:29 -0300

    chore: refactor initial migrations and add validation pipe

--- apps/clients/prisma/migrations/20240131141220_initial_migration/migration.sql
+++ apps/clients/prisma/migrations/20240131141220_initial_migration/migration.sql
@@ -4,9 +4,9 @@ CREATE TABLE `contacts` (
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
     `name` VARCHAR(191) NOT NULL,
-    `cellphone` VARCHAR(191) NOT NULL,
+    `cellphone` VARCHAR(20) NOT NULL,
     `created_by` VARCHAR(191) NOT NULL,
-    `client` VARCHAR(191) NOT NULL DEFAULT 'Macapá',
+    `client` VARCHAR(191) NOT NULL DEFAULT 'Macapa',
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
